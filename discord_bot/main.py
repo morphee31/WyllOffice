b219498f3375f6273192c0a55c81f443e6173c7b
Thomas <samoht.cassan@gmail.com>
2023-04-17 05:45:39 +0000

    Core api

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,2 +1,25 @@
-if __name__ == '__main__':
-    pass
+
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.reply('Hello!', mention_author=True)
+
+
+
+if __name__ == "__main__":
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = MyClient(intents=intents)
+    client.run('MTA3NDk5MTQ5NTMyMjU0NjE3Ng.GAxS7T.ECoDblEytoUSVk-X-igP9F08bqxw-VEcC5A3X8')
