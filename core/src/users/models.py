5c48d9885b2d6b98bb442066f988419f60c323a6
Thomas <samoht.cassan@gmail.com>
2023-04-20 05:52:01 +0000

    add planning of model

--- core/src/users/models.py
+++ core/src/users/models.py
@@ -1,12 +1,22 @@
 from pydantic import EmailStr, SecretStr
-
+from enum import Enum
 from models import BaseDBModel
+
+from typing import Literal
+
+
+
+
+class Planning(BaseDBModel):
+    day: datetime.date
+    period: Literal["am", "pm", "day"]
 
 
 class UserModel(BaseDBModel):
     email: EmailStr
     firstname: str
     lastname: str
+    planning: None | Planning = Field(default=None, description="Day of presence")
 
 
 class UserCreationModel(UserModel):
